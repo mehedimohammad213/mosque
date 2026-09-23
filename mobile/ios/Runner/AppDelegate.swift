a7e7c0c94cab1d13f16e2c73c789c74eaff57b34
mehedimohammad213 <mehedimohammad213@gmail.com>
2026-09-23 16:43:21 +0600

    remove api key

--- mobile/ios/Runner/AppDelegate.swift
+++ mobile/ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -8,7 +7,6 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCy1-bu0VregtL0weKnOLCKc2oa2KZSSP0")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
